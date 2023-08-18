c3774bd1c7816c4a771580f13fe6a011f4e87cf5
SkekyTv <skeky.stream@gmail.com>
2023-08-18 10:19:46 +0200

    feat: Init basic bot

---
+++ exemple_bot.py
@@ -0,0 +1,36 @@
+# This example requires the 'message_content' intent.
+
+import discord
+
+# Import the os module.
+import os
+
+# Import load_dotenv function from dotenv module.
+from dotenv import load_dotenv
+
+# Loads the .env file that resides on the same level as the script.
+load_dotenv()
+
+# Grab the API token from the .env file.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+print(DISCORD_TOKEN)
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(DISCORD_TOKEN)
+
+
